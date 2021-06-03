5cb7b2cc7543b580a02b30bb9d4082b2ce8d6e4f
Do Minh Khai <khaihkd@gmail.com>
2021-06-03 17:20:28 +0700

    add script update config

---
+++ updateConfig.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+const config = require('config')
+
+
+const contract_dirname = 'contracts'
+
+fs.readdirSync(path.resolve(__dirname, '..', 'contracts/deployments'))
+    .filter(function (file) {
+        return (file.indexOf('.') !== 0) && (file !== 'index.js')
+    })
+    .forEach(function (file) {
+        // console.log(file)
+        let content = require(path.join(path.resolve(__dirname, '..', contract_dirname,  'deployments', file), 'GenericBridge.json'))
+        if (config.contracts[file]) {
+            config.contracts[file].bridge = content.address
+            config.contracts[file].firstBlockCrawl = content.deployTransaction.blockNumber
+            // var model = require(path.join(__dirname, file))
+        } else {
+            config.contracts[file] = {
+                bridge: content.address,
+                firstBlockCrawl: content.deployTransaction.blockNumber
+            }
+        }
+    })
+
+// console.log('aaa', data)
+console.log(config.contracts)
+
+fs.writeFile("config/default.json", JSON.stringify(config), function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
