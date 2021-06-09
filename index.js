48109256dd5dd0449ea404c893d3c0bf3428b290
Do Minh Khai <khaihkd@gmail.com>
2021-06-09 12:13:32 +0700

    add morgan &  update package.json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const config = require('config')
 const bodyParser = require('body-parser')
 const app = express()
+const morgan = require('morgan')
 const cors = require('cors')
 const compression = require('compression')
 const logger = require('./helpers/logger')
@@ -12,6 +13,12 @@ app.use(compression())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+morgan.token('remote-addr', function (req, res) {
+    const ffHeaderValue = req.header('x-forwarded-for') ? req.header('x-forwarded-for').split(',')[0] : ''
+    return ffHeaderValue || req.connection.remoteAddress
+})
+app.use(morgan('short'))
 // api
 app.use(require('./api'))
 
