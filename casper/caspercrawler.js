d42900b504eaaea846318f925f8f00ea42e17e60
dotoracle <dotoracle.network@gmail.com>
2022-03-17 15:53:34 +0700

    speed up casper crawl

--- casper/caspercrawler.js
+++ casper/caspercrawler.js
@@ -116,6 +116,184 @@ function findArg(args, argName) {
   return args.find((e) => e[0] == argName);
 }
 
+async function crawl(from, to, lastBlockHeight) {
+  let fromBlock = from
+  let toBlock = to
+  let casperConfig = CasperHelper.getConfigInfo();
+  let client = new CasperServiceByJsonRPC(casperConfig.rpc);
+  while (fromBlock < toBlock) {
+    try {
+      let block = await client.getBlockInfoByHeight(fromBlock);
+      console.log('readding block', block.block.header.height)
+      let deploy_hashes = block.block.body.deploy_hashes;
+
+      //reading deploy hashes one by one
+      for (const h of deploy_hashes) {
+        let deployResult = await client.getDeployInfo(h);
+        let deploy = deployResult.deploy;
+        if (deployResult.execution_results) {
+          let result = deployResult.execution_results[0];
+          if (result.result.Success) {
+            //analyzing deploy details
+            let session = deploy.session;
+            if (session && session.StoredContractByHash) {
+              let StoredContractByHash = session.StoredContractByHash;
+              if (contractHashes.includes(StoredContractByHash.hash)) {
+                let tokenData = casperConfig.tokens.find(
+                  (e) => e.contractHash == StoredContractByHash.hash
+                );
+                let args = StoredContractByHash.args;
+                if (StoredContractByHash.entry_point == "mint") {
+                  let recipient = findArg(args, "recipient");
+                  let amount = findArg(args, "amount");
+                  let mintid = findArg(args, "mintid");
+                  let fee = findArg(args, "swap_fee");
+
+                  {
+                    //{
+                    //   index: eventData._index,
+                    //   fromChainId: eventData._fromChainId,
+                    //   toChainId: eventData._toChainId,
+                    // },
+                    // {
+                    //   $set: {
+                    //     claimHash: eventData.transactionHash,
+                    //     claimBlock: eventData.blockNumber,
+                    //     claimed: true,
+                    //     claimId: eventData._claimId,
+                    //   },
+                    // },
+                  }
+                  //mintid = <txHash>-<fromChainId>-<toChainId>-<index>-<originContractAddress>-<originChainId>
+                  let mintidStr = mintid[1].parsed;
+                  let mintidSplits = mintidStr.split("-");
+                  let transactionHash = h;
+                  let fromChainId = parseInt(mintidSplits[1]);
+                  let toChainId = parseInt(mintidSplits[2]);
+                  let index = parseInt(mintidSplits[3]);
+                  let originContractAddress = mintidSplits[4];
+                  let originChainId = parseInt(mintidSplits[5]);
+
+                  let blockNumber = block.block.header.height;
+                  let claimId = mintidStr;
+                  let eventData = {
+                    fromChainId: fromChainId,
+                    toChainId: toChainId,
+                    transactionHash: CasperHelper.toNormalTxHash(transactionHash),
+                    index: index,
+                    blockNumber: blockNumber,
+                    claimId: claimId,
+                    originToken: tokenData.originContractAddress.toLowerCase(),
+                    originChainId: tokenData.originChainId,
+                  };
+                  logger.info("Casper Network Minting: %s", eventData);
+
+                  await processMintEvent(
+                    networkId,
+                    block.block.header.height,
+                    lastBlockHeight,
+                    eventData
+                  );
+                } else if (StoredContractByHash.entry_point == "transfer") {
+                  console.log("transfer");
+                } else if (
+                  StoredContractByHash.entry_point == "request_bridge_back"
+                ) {
+                  let txCreator = ""
+                  if (deploy.approvals.length > 0) {
+                    txCreator = deploy.approvals[0].signer
+                    txCreator = CasperHelper.fromCasperPubkeyToAccountHash(txCreator)
+                  }
+                  let amount = findArg(args, "amount");
+                  amount = amount[1].parsed;
+                  let toChainId = findArg(args, "to_chainid");
+                  toChainId = toChainId[1].parsed;
+                  let fee = findArg(args, "fee");
+                  fee = fee[1].parsed;
+                  let receiver_address = findArg(args, "receiver_address");
+                  receiver_address = receiver_address[1].parsed;
+                  let id = findArg(args, "id");
+                  id = id[1].parsed;
+
+                  //reading index from id
+                  const erc20 = new ERC20Client(
+                    casperConfig.rpc,
+                    casperConfig.chainName,
+                    casperConfig.eventStream,
+                  )
+
+                  await erc20.setContractHash(
+                    tokenData.contractHash
+                  )
+
+                  id = await erc20.readRequestIndex(id)
+                  //amount after fee
+                  amount = new BigNumber(amount).minus(fee).toString();
+
+                  // await db.Transaction.updateOne(
+                  //   {
+                  //     index: eventData.index,
+                  //     fromChainId: eventData.fromChainId,
+                  //     toChainId: eventData.toChainId,
+                  //     originChainId: eventData.originChainId,
+                  //     originToken: eventData.originToken,
+                  //   },
+                  //   {
+                  //     $set: {
+                  //       requestHash: eventData.transactionHash,
+                  //       requestBlock: eventData.blockNumber,
+                  //       account: eventData.toAddr.toLowerCase(),
+                  //       originToken: token.hash,
+                  //       originSymbol: token.symbol,
+                  //       fromChainId: eventData.fromChainId,
+                  //       originChainId: eventData.originChainId,
+                  //       toChainId: eventData.toChainId,
+                  //       amount: amount,
+                  //       // amountNumber: amountNumber, // TODO: get token from chain detail
+                  //       index: eventData.index,
+                  //       requestTime: block.timestamp,
+                  //     },
+                  //   },
+                  //   { upsert: true, new: true }
+                  // );
+                  let timestamp = Date.parse(block.block.header.timestamp)
+                  let eventData = {
+                    token: tokenData.originContractAddress.toLowerCase(),
+                    index: parseInt(id),
+                    fromChainId: parseInt(casperConfig.networkId),
+                    toChainId: parseInt(toChainId),
+                    originChainId: tokenData.originChainId,
+                    originToken: tokenData.originContractAddress.toLowerCase(),
+                    transactionHash: CasperHelper.toNormalTxHash(h),
+                    blockNumber: block.block.header.height,
+                    toAddr: receiver_address,
+                    amount: amount,
+                    index: parseInt(id),
+                    requestTime: Math.floor(timestamp / 1000),
+                    txCreator: txCreator
+                  };
+
+                  logger.info("Casper Network Request: %s, %s", eventData);
+
+                  await processRequestEvent(
+                    block.block.header.height,
+                    lastBlockHeight,
+                    eventData
+                  );
+                }
+              }
+            }
+          }
+        }
+      }
+      fromBlock++
+    } catch (e) {
+      await generalHelper.sleep(5 * 1000)
+      client = new CasperServiceByJsonRPC(casperConfig.rpc);
+    }
+  }
+}
+
 const getPastEvent = async () => {
   let casperConfig = CasperHelper.getConfigInfo();
   let networkId = casperConfig.networkId;
@@ -127,202 +305,50 @@ const getPastEvent = async () => {
     fromBlock = setting.lastBlockRequest;
   }
 
-  let contractHashes = casperConfig.tokens.map((e) => e.contractHash);
   let currentBlock = await client.getLatestBlockInfo();
   let currentBlockHeight = parseInt(
     currentBlock.block.header.height.toString()
   );
-  while (currentBlockHeight - fromBlock > 5) {
-    //reading info
-    let block = await client.getBlockInfoByHeight(fromBlock);
-    console.log('readding block', block.block.header.height)
-    let deploy_hashes = block.block.body.deploy_hashes;
 
-    //reading deploy hashes one by one
-    for (const h of deploy_hashes) {
-      let deployResult = await client.getDeployInfo(h);
-      let deploy = deployResult.deploy;
-      if (deployResult.execution_results) {
-        let result = deployResult.execution_results[0];
-        if (result.result.Success) {
-          //analyzing deploy details
-          let session = deploy.session;
-          if (session && session.StoredContractByHash) {
-            let StoredContractByHash = session.StoredContractByHash;
-            if (contractHashes.includes(StoredContractByHash.hash)) {
-              let tokenData = casperConfig.tokens.find(
-                (e) => e.contractHash == StoredContractByHash.hash
-              );
-              let args = StoredContractByHash.args;
-              if (StoredContractByHash.entry_point == "mint") {
-                let recipient = findArg(args, "recipient");
-                let amount = findArg(args, "amount");
-                let mintid = findArg(args, "mintid");
-                let fee = findArg(args, "swap_fee");
+  currentBlockHeight -= 5
 
-                {
-                  //{
-                  //   index: eventData._index,
-                  //   fromChainId: eventData._fromChainId,
-                  //   toChainId: eventData._toChainId,
-                  // },
-                  // {
-                  //   $set: {
-                  //     claimHash: eventData.transactionHash,
-                  //     claimBlock: eventData.blockNumber,
-                  //     claimed: true,
-                  //     claimId: eventData._claimId,
-                  //   },
-                  // },
-                }
-                //mintid = <txHash>-<fromChainId>-<toChainId>-<index>-<originContractAddress>-<originChainId>
-                let mintidStr = mintid[1].parsed;
-                let mintidSplits = mintidStr.split("-");
-                let transactionHash = h;
-                let fromChainId = parseInt(mintidSplits[1]);
-                let toChainId = parseInt(mintidSplits[2]);
-                let index = parseInt(mintidSplits[3]);
-                let originContractAddress = mintidSplits[4];
-                let originChainId = parseInt(mintidSplits[5]);
-
-                let blockNumber = block.block.header.height;
-                let claimId = mintidStr;
-                let eventData = {
-                  fromChainId: fromChainId,
-                  toChainId: toChainId,
-                  transactionHash: CasperHelper.toNormalTxHash(transactionHash),
-                  index: index,
-                  blockNumber: blockNumber,
-                  claimId: claimId,
-                  originToken: tokenData.originContractAddress.toLowerCase(),
-                  originChainId: tokenData.originChainId,
-                };
-                logger.info("Casper Network Minting: %s", eventData);
-
-                await processMintEvent(
-                  networkId,
-                  block.block.header.height,
-                  currentBlock.block.header.height,
-                  eventData
-                );
-              } else if (StoredContractByHash.entry_point == "transfer") {
-                console.log("transfer");
-              } else if (
-                StoredContractByHash.entry_point == "request_bridge_back"
-              ) {
-                let txCreator = ""
-                if (deploy.approvals.length > 0) {
-                  txCreator = deploy.approvals[0].signer
-                  txCreator = CasperHelper.fromCasperPubkeyToAccountHash(txCreator)
-                }
-                let amount = findArg(args, "amount");
-                amount = amount[1].parsed;
-                let toChainId = findArg(args, "to_chainid");
-                toChainId = toChainId[1].parsed;
-                let fee = findArg(args, "fee");
-                fee = fee[1].parsed;
-                let receiver_address = findArg(args, "receiver_address");
-                receiver_address = receiver_address[1].parsed;
-                let id = findArg(args, "id");
-                id = id[1].parsed;
-
-                //reading index from id
-                const erc20 = new ERC20Client(
-                  casperConfig.rpc,
-                  casperConfig.chainName,
-                  casperConfig.eventStream,
-                )
-              
-                await erc20.setContractHash(
-                  tokenData.contractHash
-                )
-
-                id = await erc20.readRequestIndex(id)
-                //amount after fee
-                amount = new BigNumber(amount).minus(fee).toString();
-
-                // await db.Transaction.updateOne(
-                //   {
-                //     index: eventData.index,
-                //     fromChainId: eventData.fromChainId,
-                //     toChainId: eventData.toChainId,
-                //     originChainId: eventData.originChainId,
-                //     originToken: eventData.originToken,
-                //   },
-                //   {
-                //     $set: {
-                //       requestHash: eventData.transactionHash,
-                //       requestBlock: eventData.blockNumber,
-                //       account: eventData.toAddr.toLowerCase(),
-                //       originToken: token.hash,
-                //       originSymbol: token.symbol,
-                //       fromChainId: eventData.fromChainId,
-                //       originChainId: eventData.originChainId,
-                //       toChainId: eventData.toChainId,
-                //       amount: amount,
-                //       // amountNumber: amountNumber, // TODO: get token from chain detail
-                //       index: eventData.index,
-                //       requestTime: block.timestamp,
-                //     },
-                //   },
-                //   { upsert: true, new: true }
-                // );
-                let timestamp = Date.parse(block.block.header.timestamp)
-                let eventData = {
-                  token: tokenData.originContractAddress.toLowerCase(),
-                  index: parseInt(id),
-                  fromChainId: parseInt(casperConfig.networkId),
-                  toChainId: parseInt(toChainId),
-                  originChainId: tokenData.originChainId,
-                  originToken: tokenData.originContractAddress.toLowerCase(),
-                  transactionHash: CasperHelper.toNormalTxHash(h),
-                  blockNumber: block.block.header.height,
-                  toAddr: receiver_address,
-                  amount: amount,
-                  index: parseInt(id),
-                  requestTime: Math.floor(timestamp / 1000),
-                  txCreator: txCreator
-                };
-                
-                logger.info("Casper Network Request: %s, %s", eventData);
-
-                await processRequestEvent(
-                  block.block.header.height,
-                  currentBlock.block.header.height,
-                  eventData
-                );
-              }
-            }
-          }
-        }
-      }
+  let blockPerBatch = 500
+  let numBatch = Math.floor((currentBlockHeight - fromBlock) / blockPerBatch) + 1
+  let tasks = []
+  for (var i = 0; i < numBatch; i++) {
+    let from = fromBlock + i * blockPerBatch
+    let to = fromBlock + (i + 1) * blockPerBatch
+    if (to > currentBlockHeight) {
+      to = currentBlockHeight
     }
-    let blockNumber = block.block.header.height
-    let setting = await db.Setting.findOne({ networkId: networkId });
-    if (!setting) {
-      await db.Setting.updateOne(
-        { networkId: networkId },
-        { $set: { lastBlockClaim: blockNumber, lastBlockRequest: blockNumber } },
-        {
-          upsert: true,
-          new: true,
-        }
-      );
-    } else {
-      if (blockNumber > setting.lastBlockRequest) {
-        setting.lastBlockRequest = blockNumber;
-        setting.lastBlockClaim = blockNumber;
-        await setting.save();
+    tasks.push(crawl(from, to, currentBlockHeight))
+  }
+  await Promise.all(tasks)
+
+  let blockNumber = currentBlockHeight
+  setting = await db.Setting.findOne({ networkId: networkId });
+  if (!setting) {
+    await db.Setting.updateOne(
+      { networkId: networkId },
+      { $set: { lastBlockClaim: blockNumber, lastBlockRequest: blockNumber } },
+      {
+        upsert: true,
+        new: true,
       }
+    );
+  } else {
+    if (blockNumber > setting.lastBlockRequest) {
+      setting.lastBlockRequest = blockNumber;
+      setting.lastBlockClaim = blockNumber;
+      await setting.save();
     }
-    fromBlock++;
   }
 };
 
 let watch = async () => {
   while (true) {
     await getPastEvent();
-    generalHelper.sleep(10 * 1000)
+    await generalHelper.sleep(10 * 1000)
   }
 };
 
