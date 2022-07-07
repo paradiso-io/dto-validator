37f127dadf83f72d4875a4de104bcacf284fce95
dotoracle <dotoracle.network@gmail.com>
2022-07-07 17:35:40 +0700

    add tokenmap table

---
+++ models/tokenMap.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TokenMap = new Schema({
+    address: { type: String, index: true }, //origin contract address
+    networkId: {type: Number, index: String},   //origin network Id
+    name: String,
+    symbol: String,
+    decimals: Number,
+    mapInfo: Object
+}, { timestamps: false, v: false })
+
+module.exports = mongoose.model('TokenMap', TokenMap)
