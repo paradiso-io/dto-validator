2281ebe5d08d2e39fa4c0cb31e543950df2bca12
Do Minh Khai <khaihkd@gmail.com>
2022-08-17 22:44:49 +0700

    fix bug nft api

--- scripts/bridgeNFT/claim721ByApi.js
+++ scripts/bridgeNFT/claim721ByApi.js
@@ -5,24 +5,22 @@ const Web3 = require("web3");
 const ERC721 = require("./erc721.json");
 const BridgeABI = require("./nft721.json");
 const PrivateKeyProvider = require("truffle-privatekey-provider");
-const erc20Address = "0x9838c4943c203a8478b21e233e4df3baaeed8770"
 const axios = require('axios')
 
-console.log("to:", erc20Address);
 const privateKey = '0xe9a698185c72b3cebc14d68213e5ead83efdc784ee93f34a0e2e76ba68b07727'
-// const rpc = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
-const rpc = 'https://rinkeby.infura.io/v3/99d4e19c704546fbbbdfe408d354b9c8';
-const bridgeAddress = '0x9de8C0Bc516c26e3Eb25c4C8995c5636084fB4C8'
-const nftToken = '0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b';
+const rpc = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
+// const rpc = 'https://rinkeby.infura.io/v3/99d4e19c704546fbbbdfe408d354b9c8';
+const bridgeAddress = '0xB4D46B3ffd8079bd2b5d69A826bb3014b4153bB4'
+const nftToken = '0xf5de760f2e916647fd766b4ad9e85ff943ce3a2b';
 
 
-let tokenIds = [1777197]
+let tokenIds = [1777196]
 let fromChainId = 42
-let toChainId = 4
-let index = 1
+let toChainId = 97
+let index = 0
 
 let chainIdIndex = [fromChainId, fromChainId, toChainId, index]
-let txHash = '0x8adf4ab2dd19f6f19e67a197a3c14c2a81198b4396434ddd4ddeb0fa8f4dbae0'
+let txHash = '0x6d245c366b3f879a7957b799bf25ceacdbba5ee88d7e2beacd61a24349967b74'
 
 async function claimBridge() {
   try {
@@ -49,12 +47,12 @@ async function claimBridge() {
       .claimMultiNFT721Token(nftToken, mainAccount, tokenIds, chainIdIndex,
         txHash,
         data.r, data.s, data.v,
-        data.name, data.symbol)
+        data.name, data.symbol, data.tokenUris)
       .send({
-        chainId: 4,
-        // chainId: 97,
+        // chainId: 4,
+        chainId: 97,
         from: mainAccount,
-        gas: 9000000
+        // gas: 9000000
       });
 
     console.log("done");
