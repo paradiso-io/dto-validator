c22f7045f82901ca7f9f9d9daad10390aa7b0f3d
dotoracle <dotoracle.network@gmail.com>
2022-09-06 16:57:39 +0700

    fix issue when encoding origin contract address

--- api/nft721.js
+++ api/nft721.js
@@ -401,10 +401,27 @@ router.post('/request-withdraw', [
                 tokenUris.push(tokenUri)
             }
         } else {
-            for (let i = 0; i < tokenIds.length; i++) {
-                let tokenUri = JSON.parse(transaction.tokenMetadatas[i])
-                tokenUri = tokenUri.token_uri
-                tokenUris.push(tokenUri)
+            if (fromChainId == casperConfig.networkId) {
+                for (let i = 0; i < tokenIds.length; i++) {
+                    let tokenUri = JSON.parse(transaction.tokenMetadatas[i])
+                    tokenUri = tokenUri.token_uri
+                    tokenUris.push(tokenUri)
+                }
+            } else {
+                let web3Origin = await Web3Utils.getWeb3(transaction.fromChainId)
+                for (let i = 0; i < tokenIds.length; i++) {
+                    let tokenUri = await GeneralHelper.tryCallWithTrial(async () => {
+                        let contract = await Web3Utils.getNft721BridgeContract(fromChainId)
+                        let tokenAddress = await contract.methods.tokenMap(casperConfig.networkId, transaction.originToken).call()
+                        let tokenContract = await new web3Origin.eth.Contract(ERC721, tokenAddress)
+                        let tokenUri = await tokenContract.methods.tokenURI(tokenIds[i]).call()
+                        return tokenUri
+                    })
+                    if (tokenUri == undefined) {
+                        return res.status(400).json({ errors: 'Failed to read token metadata, please try again later' })
+                    }
+                    tokenUris.push(tokenUri)
+                }
             }
 
             let nftConfig = CasperHelper.getNFTConfig()
