5588d236191bcb3c265e9ad01f66e5640b824c5a
tonititi <dong.vu.cong@gmail.com>
2022-09-08 17:09:03 +0700

    edit api goodRPC

--- api/main.js
+++ api/main.js
@@ -246,9 +246,12 @@ router.get('/verify-transaction/:requestHash/:fromChainId/:index', [
         }
     } else {
         //casper
-        let casperRPC = CasperHelper.getCasperRPC()
         try {
             transaction = await db.Transaction.findOne({ requestHash: requestHash, fromChainId: fromChainId })
+            if (!transaction) {
+                return res.json({ success: false })
+            }
+            let casperRPC = await CasperHelper.getCasperRPC(transaction.requestBlock)
             let deployResult = await casperRPC.getDeployInfo(CasperHelper.toCasperDeployHash(transaction.requestHash))
             let eventData = await CasperHelper.parseRequestFromCasper(deployResult)
             if (eventData.toAddr.toLowerCase() != transaction.account.toLowerCase()
